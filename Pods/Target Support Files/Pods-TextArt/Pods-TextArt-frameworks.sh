42adffd289e8383845620e618baa916ca983a7f3
ti_shawon <1405050.ti@ugrad.cse.buet.ac.bd>
2022-09-18 01:56:45 +0600

    Add SwiftHSVColorPicker
    Need to add this file for color picker

--- Pods/Target Support Files/Pods-TextArt/Pods-TextArt-frameworks.sh
+++ Pods/Target Support Files/Pods-TextArt/Pods-TextArt-frameworks.sh
@@ -177,9 +177,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MetalPetal/MetalPetal.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftHSVColorPicker/SwiftHSVColorPicker.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MetalPetal/MetalPetal.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftHSVColorPicker/SwiftHSVColorPicker.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
