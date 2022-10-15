939cf7533f2ecc079278b0ac9a3f119ada2e7dd2
sadiqulamin <113623819+sadiqulamin@users.noreply.github.com>
2022-10-16 02:56:46 +0600

    ...
    fixed

--- TextArt/AppDelegate.swift
+++ TextArt/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         DBmanager.shared.initDB()
         
+       
+        
+    
+        
         let documentDirectoryUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
         let fileUrl = documentDirectoryUrl?.appendingPathComponent("wallpapers.json")
         
