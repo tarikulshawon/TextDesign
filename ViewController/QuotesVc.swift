4d6b762b6d6876cc30c8ebc234d220611c587bb6
ti_shawon <1405050.ti@ugrad.cse.buet.ac.bd>
2022-10-16 04:08:23 +0600

    quotes tableView

--- ViewController/QuotesVc.swift
+++ ViewController/QuotesVc.swift
@@ -13,8 +13,13 @@ protocol quotesDelegate: AnyObject {
 
 class QuotesVc: UIViewController,UITableViewDelegate, UITableViewDataSource {
     weak var delegateForQuotes: quotesDelegate?
-    var quotesArray:NSArray!
+    var quotesArray: NSArray!
     var headerName = [String]()
+    var quotesDic = [String: NSArray]()
+    
+    private var hiddenSections = Set<Int>()
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBAction func gotoPreviousvIEW(_ sender: Any) {
         self.dismiss(animated: true)
@@ -25,60 +30,51 @@ class QuotesVc: UIViewController,UITableViewDelegate, UITableViewDataSource {
         let path = Bundle.main.path(forResource: "Motivational", ofType: "plist")
         quotesArray = NSArray(contentsOfFile: path!)
         
-        for ma in quotesArray {
-            
-            if let a = ma as? Dictionary<String, Any> ,let b = a["types"] as? String{
-                headerName.append(b)
+        quotesArray.forEach {
+            if let dictionary = $0 as? Dictionary<String, Any>,
+               let sectionName = dictionary["types"] as? String,
+               let contents = dictionary["items"] as? NSArray
+            {
+                headerName.append(sectionName)
+                quotesDic[sectionName] = contents
             }
-            
-            
         }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let sectionButton = UIButton()
+        sectionButton.setTitle(headerName[section], for: .normal)
+        sectionButton.setTitleColor(.black, for: .normal)
+        sectionButton.layer.cornerRadius = 10
         
-        // Do any additional setup after loading the view.
+        sectionButton.backgroundColor = .systemGray5
+        sectionButton.tag = section
+        sectionButton.addTarget(self,
+                                action: #selector(self.hideSection(sender:)),
+                                for: .touchUpInside)
+        
+        return sectionButton
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int { quotesDic.keys.count }
     
-    //    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-    //
-    //        let headerView = UIView()
-    //        headerView.backgroundColor = UIColor.lightGray.withAlphaComponent(1.0)
-    //        let headerLabel = UILabel(frame: CGRect(x: 30, y: 0, width:
-    //        tableView.bounds.size.width, height: tableView.bounds.size.height))
-    //        headerLabel.font = UIFont.systemFont(ofSize: 20)
-    //        headerLabel.textColor = UIColor.white
-    //        headerLabel.text = headerName[section]
-    //        headerLabel.sizeToFit()
-    //        headerView.addSubview(headerLabel)
-    //        return headerView
-    //    }
-    
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return  quotesArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 40
-    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { 40 }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var lol = quotesArray[section]
-        if let a = lol as? Dictionary<String, Any> ,let b = a["items"] as? NSArray{
-            return b.count
-        }
-        return 0
+        
+        if self.hiddenSections.contains(section) { return 0 }
+        
+        return quotesDic[headerName[section]]?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "QuotesCell") as? QuotesCell else {
             return QuotesCell()
         }
+
+        guard let arr = quotesDic[headerName[indexPath.section]] else { return QuotesCell() }
         
-        var lol = quotesArray[indexPath.section]
-        if let a = lol as? Dictionary<String, Any> ,let b = a["items"] as? NSArray{
-            cell.quotesL.text = b[indexPath.row] as? String
-        }
+        cell.quotesL.text = arr[indexPath.row] as? String
         
         return cell
         
@@ -88,35 +84,41 @@ class QuotesVc: UIViewController,UITableViewDelegate, UITableViewDataSource {
         return 0.1
     }
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        headerName[section]
-    }
-    
-    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
-        30
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt
-                   indexPath: IndexPath){
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let arr = quotesDic[headerName[indexPath.section]] else { return }
         
-        var lol = quotesArray[indexPath.section]
-        if let a = lol as? Dictionary<String, Any> ,let b = a["items"] as? NSArray{
-            
-            self.delegateForQuotes?.sendText1(text: b[indexPath.row] as? String ?? "")
-        }
-        //you
+        self.delegateForQuotes?.sendText1(text: arr[indexPath.row] as? String ?? "")
         
         self.dismiss(animated: true)
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+}
+
+extension QuotesVc {
+    @objc
+    private func hideSection(sender: UIButton) {
+        let section = sender.tag
+        
+        func indexPathsForSection() -> [IndexPath] {
+            var indexPaths = [IndexPath]()
+            
+            let dic = quotesArray[section] as! Dictionary<String, Any>
+            let data = dic["items"] as! NSArray
+            
+            for row in 0..<data.count {
+                indexPaths.append(IndexPath(row: row, section: section))
+            }
+            
+            return indexPaths
+        }
+        
+        if self.hiddenSections.contains(section) {
+            self.hiddenSections.remove(section)
+            self.tableView.insertRows(at: indexPathsForSection(),
+                                      with: .fade)
+        } else {
+            self.hiddenSections.insert(section)
+            self.tableView.deleteRows(at: indexPathsForSection(),
+                                      with: .fade)
+        }
+    }
 }
